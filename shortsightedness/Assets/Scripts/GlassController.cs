fc66f30139fd6e501a39d6d019ba242301a36891
Ryan Tung <53037140+13126789556@users.noreply.github.com>
2020-01-14 23:11:48 -0800

    implement glasses effect

---
+++ shortsightedness/Assets/Scripts/GlassController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassController : MonoBehaviour
+{
+    [Range(0,1)]
+    public float position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<CanvasRenderer>().GetMaterial(0).SetTextureOffset("_MainTex", new Vector2(0, -position));
+        transform.position = new Vector3(transform.position.x, 540 - position * 1080, transform.position.z);
+    }
+}
